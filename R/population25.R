0d1b4dc78f7cc3a9ff64bc5e447780c9576a2c33
Douglas Munro <douglas@douglasmunro.co.uk>
2026-05-31 17:46:04 +0000

    docs: update datasets source and add details section

--- R/population25.R
+++ R/population25.R
@@ -3,6 +3,12 @@
 #' This dataset contains the population denominators used to create the Indices of Deprivation 2025 (IoD25).
 #'  The populations are derived from the mid-2022 ONS population estimates.
 #'
+#' @details
+#' The dataset contains Ministry of Housing, Communities and Local Government (MHCLG) IoD data,
+#'    provided in a '[tidy data](https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html)' format.
+#'    Contains public sector information licensed under the [Open Government Licence v3.0](https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/?utm_source=copilot.com).
+#'
+#' 
 #' @format A tibble with 135,020 rows and 6 variables:
 #' \describe{
 #'   \item{lsoa_code}{Identifiers of 2021 Census Geography boundaries (Lower Layer Super Output Areas)}
@@ -12,5 +18,5 @@
 #'   \item{population_group}{Population group}
 #'   \item{population}{Population}
 #' }
-#' @source MHCLG: [All scores, ranks and deciles](https://open-data.communities.gov.uk/datasets/indices-of-deprivation-2025/csvw/)
+#' @source MHCLG, [All scores, ranks and deciles](https://open-data.communities.gov.uk/datasets/indices-of-deprivation-2025/csvw/).
 "population25"
