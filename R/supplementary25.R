2afee0274189ce1cd172f25273805fa990537a5f
Douglas Munro <douglas@douglasmunro.co.uk>
2026-05-31 19:16:57 +0000

    feat: add supplementary indices dataset

---
+++ R/supplementary25.R
@@ -0,0 +1,24 @@
+#' Supplementary indices of deprivation 2025
+#'
+#' This dataset contains the supplementary indices of deprivation at Lower-layer Super Output Area (LSOA) level.
+#'   The supplementary indices are the Income Deprivation Affecting Children Index (IDACI) and the Income Deprivation Affecting Older People Index (IDAOPI).
+#'
+#' @details
+#' The dataset contains Ministry of Housing, Communities and Local Government (MHCLG) IoD data,
+#'    provided in a '[tidy data](https://cran.r-project.org/web/packages/tidyr/vignettes/tidy-data.html)' format.
+#'    Contains public sector information licensed under the [Open Government Licence v3.0](https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/?utm_source=copilot.com).
+#'
+#'
+#' @format A tibble with 67,510 rows and 8 variables:
+#' \describe{
+#'   \item{lsoa_code}{Identifiers of 2021 Census Geography boundaries (Lower Layer Super Output Areas)}
+#'   \item{lsoa_name}{Names of 2021 Census Geography boundaries (Lower Layer Super Output Areas)}
+#'   \item{lad_code}{Identifiers for local authority districts (LAD) and unitary authorities (UA) in the United Kingdom as at 2024}
+#'   \item{lad_name}{Names for local authority districts (LAD) and unitary authorities (UA) in the United Kingdom as at 2024}
+#'   \item{index}{Deprivation Index}
+#'   \item{rank}{Index Rank (where 1 is most deprived)}
+#'   \item{decile}{Index Decile (where 1 is most deprived 10% of LSOAs)}
+#'   \item{score}{Index Score}
+#' }
+#' @source MHCLG, [All scores, ranks and deciles](https://open-data.communities.gov.uk/datasets/indices-of-deprivation-2025/csvw/).
+"supplementary25"
---
+++ data-raw/supplementary25.R
@@ -0,0 +1,30 @@
+path <- "./data-raw/2025_all_iod_scores_ranks_deciles.csv"
+
+supplementary25 <- vroom::vroom(
+  path,
+  col_select = c("lsoa_code":"local_authority_district_name", "idaci_score":"idaopi_decile"),
+  .name_repair = janitor::make_clean_names,
+  show_col_types = FALSE
+) |>
+  dplyr::rename(
+    lad_code = local_authority_district_code,
+    lad_name = local_authority_district_name,
+  ) |> 
+   tidyr::pivot_longer(
+    cols = !c("lsoa_code":"lad_name"),
+    names_to = c("index", "type"),
+    names_pattern = "(.*)_(.*)"
+  ) |>
+  tidyr::pivot_wider(
+    names_from = "type",
+    values_from = "value"
+  ) |>
+  dplyr::mutate(
+    rank = as.integer(rank),
+    decile = as.integer(decile),
+    index = stringr::str_to_upper(index) |>
+      as.factor()
+  ) |>
+  dplyr::relocate("score", .after = "decile")
+
+usethis::use_data(supplementary25, overwrite = TRUE)
