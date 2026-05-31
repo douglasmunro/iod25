71588dccd433f99e02172c46831d3950e3bc3334
Douglas Munro <douglas@douglasmunro.co.uk>
2026-05-31 16:50:23 +0000

    feat: add IMD score column

--- R/imd25.R
+++ R/imd25.R
@@ -11,6 +11,7 @@
 #'   \item{lad_name}{Names for local authority districts (LAD) and unitary authorities (UA) in the United Kingdom as at 2024}
 #'   \item{rank}{Index of Multiple Deprivation (IMD) Rank (where 1 is most deprived)}
 #'   \item{decile}{Index of Multiple Deprivation (IMD) Decile (where 1 is most deprived 10% of LSOAs)")}
+#'   \item{score}{Index of Multiple Deprivation (IMD) Score")}
 #' }
 #' @source MHCLG: [Index of Multiple Deprivation](https://open-data.communities.gov.uk/datasets/indices-of-deprivation-2025/)
 "imd25"
